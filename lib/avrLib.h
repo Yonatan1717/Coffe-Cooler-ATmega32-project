82e44a56cfce491a852ce9dadc0f78ecfa0e04ef
Y <nanikanblidet@gmail.com>
2025-03-06 18:45:25 +0100

    funksjon ADC prescaler selections

--- lib/avrLib.h
+++ lib/avrLib.h
@@ -2,4 +2,9 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
-void ADC_Prescaler_Selections(u_int8)
+void ADC_Prescaler_Selections(uint8_t bit){
+    // side 217, Table 85
+
+    if(bit == 8) ADCSRA |= (1<<ADPS1) | (1<<ADPS0);
+    else ADCSRA |= (1<<ADPS2);
+}
