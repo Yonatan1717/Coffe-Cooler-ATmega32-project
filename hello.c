149aff40e65eee9213e2c4ccae89e6d483a26764
Y <nanikanblidet@gmail.com>
2025-03-06 19:25:07 +0100

    lib enderinger test 2

--- hello.c
+++ hello.c
@@ -1,18 +1,6 @@
 #include <stdio.h>
 
 int main(){
-    for (int  i = 4294967279; i < 200; i-17)
-    {   unsigned char hello = i;
-        printf("%i\n", hello);
-    }
-
-
-
-
-    
-    
-    
-    
-    // hola
+    // test
     return 0;
 }
