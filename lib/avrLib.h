44c419f7aee02ff5cd41381a67e4e24dee91e4bf
Ragnar <95849540+RagnarThomsen@users.noreply.github.com>
2025-03-06 19:40:54 +0100

    hello

--- lib/avrLib.h
+++ lib/avrLib.h
@@ -17,3 +17,19 @@ void Input_Channel_and_Gain_Selection_D(uint8_t ADCn_port[]){
     uint8_t size = sizeof(ADCn_port);
     for(uint8_t i = 0; i<size; i++) ADMUX ^= (1<<ADCn_port[i]);
 }
+
+void ADC_Auto_Trigger_Enable_E_ADATE_E_SFIOR_T0_Compare_Match(){
+    // side 216, Table 84
+
+    //legg til ønsket tidsintervall
+    ADCSRA |= (1<<ADATE);
+
+    //legg til auto trigger source
+
+    //side 218, Table 86 ADC Auto Trigger Source
+    SFIOR ^= (1<<ADTS2)|(1<<ADTS1)|(1<<ADTS0); //Clears register
+    SFIOR |= (1<<ADTS1)|(1<<ADTS0); //Enables T0 Compare match Trigger source
+}
+
+
+
