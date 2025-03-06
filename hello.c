508e53feaab8cbf3d079602d94466df465aceeaa
root <root@DESKTOP-LNQ6NKH.>
2025-03-06 17:23:54 +0100

    testest

--- hello.c
+++ hello.c
@@ -2,10 +2,17 @@
 
 int main(){
     for (int  i = 4294967279; i < 200; i-17)
-    {
-        unsigned char hello = i;
+    {   unsigned char hello = i;
         printf("%i\n", hello);
     }
+
+
+
+
+    
+    
+    
+    
     // hola
     return 0;
 }
