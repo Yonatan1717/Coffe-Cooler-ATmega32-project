f950c078e00b384182ca58704383ccc8ca8f07be
Y <nanikanblidet@gmail.com>
2025-03-06 20:53:38 +0100

    clock select prescaler funcksjon adda

--- lib/avrLib.h
+++ lib/avrLib.h
@@ -1,6 +1,7 @@
 // biblotek for avr prosjekter
 #include <avr/io.h>
 #include <avr/interrupt.h>
+#include <stdlib.h>
 
 void ADC_Prescaler_Selections(uint8_t bit){
     // side 217, Table 85
@@ -9,13 +10,17 @@ void ADC_Prescaler_Selections(uint8_t bit){
     else ADCSRA |= (1<<ADPS2);
 }
 
-void Input_Channel_and_Gain_Selection_E(uint8_t ADCn_port[]){
-    uint8_t size = sizeof(ADCn_port);
-    for(uint8_t i = 0; i<size; i++) ADMUX |= (1<<ADCn_port[i]);
+void Input_Channel_and_Gain_Selection_E(uint8_t ADCn_porter_du_Onsker_Aktivert[]){
+    // side 124, tabell 84, kan også brukes til å aktivere alle andre bits i ADMUX
+
+    uint8_t size = sizeof(ADCn_porter_du_Onsker_Aktivert);
+    for(uint8_t i = 0; i<size; i++) ADMUX |= (1<<ADCn_porter_du_Onsker_Aktivert[i]);
 }
-void Input_Channel_and_Gain_Selection_D(uint8_t ADCn_port[]){
-    uint8_t size = sizeof(ADCn_port);
-    for(uint8_t i = 0; i<size; i++) ADMUX ^= (1<<ADCn_port[i]);
+void Input_Channel_and_Gain_Selection_D(uint8_t ADCn_porter_du_Onsker_Deaktivert[]){
+    // side 125, tabell 84, kan også brukes til å deaktivere alle andre bits i ADMUX
+
+    uint8_t size = sizeof(ADCn_porter_du_Onsker_Deaktivert);
+    for(uint8_t i = 0; i<size; i++) ADMUX ^= (1<<ADCn_porter_du_Onsker_Deaktivert[i]);
 }
 
 void ADC_Auto_Trigger_Enable_E_ADATE_E_SFIOR_T0_Compare_Match(){
@@ -29,6 +34,71 @@ void ADC_Auto_Trigger_Enable_E_ADATE_E_SFIOR_T0_Compare_Match(){
     //side 218, Table 86 ADC Auto Trigger Source
     SFIOR ^= (1<<ADTS2)|(1<<ADTS1)|(1<<ADTS0); //Clears register
     SFIOR |= (1<<ADTS1)|(1<<ADTS0); //Enables T0 Compare match Trigger source
+}
+
+int Clock_Select_Description_for_a_Timer_Clock_n(uint8_t timer_clock_num, uint16_t bit_description){
+    // side 127 tabell 54 for clock 2
+    // side 110 tabell 48 for clock 1
+    // side 82 tabell 42 for clock 0
+
+    // exit status code 1: ugyldig timer/clock Number
+    // exit status code 2: ugyldig bit description
+    if(timer_clock_num != 0 && timer_clock_num != 1 && timer_clock_num != 2){
+        exit(1);
+    }
+
+    switch (bit_description)
+    {
+        case 1:
+            if(timer_clock_num == 0){
+                TCCR0 |= (1<<CS00);
+            }else if(timer_clock_num == 1){
+                TCCR1B |= (1<<CS10);
+            }else if(timer_clock_num == 2){
+                TCCR2 |= (1<<CS20);
+            }
+            break;
+        case 8:
+            if(timer_clock_num == 0){
+                TCCR0 |= (1<<CS01);
+            }else if(timer_clock_num == 1){
+                TCCR1B |= (1<<CS11);
+            }else if(timer_clock_num == 2){
+                TCCR2 |= (1<<CS21);
+            }
+            break;
+        case 64:
+            if(timer_clock_num == 0){
+                TCCR0 |= (1<<CS01) |(1<<CS00);
+            }else if(timer_clock_num == 1){
+                TCCR1B |= (1<<CS10) | (1<< CS11);
+            }else if(timer_clock_num == 2){
+                TCCR2 |= (1<<CS22);
+            }
+            break;
+        case 256:
+            if(timer_clock_num == 0){
+                TCCR0 |= (1<<CS02);
+            }else if(timer_clock_num == 1){
+                TCCR1B |= (1<<CS12);
+            }else if(timer_clock_num == 2){
+                TCCR2 |= (1<<CS22) | (1<<CS21);
+            }
+            break;
+        case 1024:
+            if(timer_clock_num == 0){
+                TCCR0 |= (1<<CS00) | (1<<CS02);
+            }else if(timer_clock_num == 1){
+                TCCR1B |= (1<<CS10) | (1<<CS12);
+            }else if(timer_clock_num == 2){
+                TCCR2 |= (1<<CS20) | (1<<CS21) | (1<<CS22);
+            }
+            break;
+        default:
+            exit(2);
+    }
+
+    return bit_description;
 }
 
 
