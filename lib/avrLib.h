f3a89f0bdafe228895bc3ba9b512f42ee21f01c0
Y <nanikanblidet@gmail.com>
2025-03-06 19:21:07 +0100

    lib enderinger

--- lib/avrLib.h
+++ lib/avrLib.h
@@ -8,3 +8,12 @@ void ADC_Prescaler_Selections(uint8_t bit){
     if(bit == 8) ADCSRA |= (1<<ADPS1) | (1<<ADPS0);
     else ADCSRA |= (1<<ADPS2);
 }
+
+void Input_Channel_and_Gain_Selection_E(uint8_t ADCn_port[]){
+    uint8_t size = sizeof(ADCn_port);
+    for(uint8_t i = 0; i<size; i++) ADMUX |= (1<<ADCn_port[i]);
+}
+void Input_Channel_and_Gain_Selection_D(uint8_t ADCn_port[]){
+    uint8_t size = sizeof(ADCn_port);
+    for(uint8_t i = 0; i<size; i++) ADMUX ^= (1<<ADCn_port[i]);
+}
